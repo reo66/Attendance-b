7c6b7a19ead95d9f0869f74a3abd387a2cf12c8c
EC2 Default User <ec2-user@ip-172-31-39-96.ap-northeast-1.compute.internal>
2020-07-11 14:27:02 +0000

    ページタイトル表示完了

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+# ページ名があるか否かでタイトルを条件分岐  
+  def full_title(page_name = "")
+    base_title = "Attendance.B"
+    if page_name.empty?
+      base_title
+    else
+       page_name + " | " + base_title
+    end
+  end  
 end
