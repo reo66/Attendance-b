1980a64a8804a1a0e7e28045704e370b18882755
EC2 Default User <ec2-user@ip-172-31-39-96.ap-northeast-1.compute.internal>
2020-07-16 13:43:13 +0000

    log_in表示OK

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
  root 'static_pages#top'
 #  新規登録
   get '/signup', to: 'users#new'
-  get 'login' , to: 'sessions#'
+#   ログイン
+  get '/login' , to: 'sessions#new'
   
   resources :users
 
