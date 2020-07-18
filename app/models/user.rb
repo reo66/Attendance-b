a579caa5e6a0ad66d6f77d4fa91928bc895e763e
EC2 Default User <ec2-user@ip-172-31-39-96.ap-northeast-1.compute.internal>
2020-07-18 15:03:10 +0000

    User.rb validates記入

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  # 文字数制限かつ、空白で保存できない様に検証をかける
+  validates :name,precence: true, length: {maximum: 50 }
+  validates :email,presence: true, length: { maximum: 100 }
 end
