53a253403b00269cfb43b4d6eaafcb28e3eb2f50
EC2 Default User <ec2-user@ip-172-31-39-96.ap-northeast-1.compute.internal>
2020-08-01 00:23:04 +0000

    アカウント作成エラ〜メッセージ表示OK

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,21 @@ class UsersController < ApplicationController
   end
   
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-    else
-    flash.now[:danger] = '認証に失敗しました。'
-    render :new
-    end
+        @user = User.new(user_params)
+      if @user.save
+        log_in @user # 保存成功後、ログインします。
+        flash[:success] = '新規作成に成功しました。'
+        redirect_to @user
+      else
+        render :new
+      end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name,:password,:password_Confiramation)
   end  
   
 end
+
