0766aafe42c14e10f9bb5debc44292299bb432ae
EC2 Default User <ec2-user@ip-172-31-39-96.ap-northeast-1.compute.internal>
2020-08-01 01:48:47 +0000

    ユーザー新規作成OK

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:name,:password,:password_Confiramation)
+     params.require(:user).permit(:name, :email, :password, :password_Confirmation)
   end  
   
 end
