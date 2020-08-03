182beb65012918e4d6d38f790f0c427bb02911f1
EC2 Default User <ec2-user@ip-172-31-39-96.ap-northeast-1.compute.internal>
2020-08-03 06:14:55 +0000

    rails.seedにadminm権限追加

--- db/migrate/20200803060313_add_admin_to_users.rb
+++ db/migrate/20200803060313_add_admin_to_users.rb
@@ -1,5 +1,6 @@
 class AddAdminToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :admin, :boolean
+    # userが管理権限を持つかをtrueかfalseで返すbooleanで判定。デフォルトをfalseに指定
+    add_column :users, :admin, :boolean,default: false
   end
 end
