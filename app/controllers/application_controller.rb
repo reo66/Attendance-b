14c47995c6acc0334f125edc98df09f7bc226e98
EC2 Default User <ec2-user@ip-172-31-39-96.ap-northeast-1.compute.internal>
2020-08-07 05:45:47 +0000

    strongparamater_basic_info_paramas 追加ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,23 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
   
   $days_of_the_week = %w{日 月 火 水 木 金 土}
+  
+  
+  def set_user
+    @user = User.find(params[:id])
+  end  
+  
+  # ログインしているか？
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "ログインしてください。"
+      redirect_to login_url
+    end
+  end
+  
+    def correct_user
+      # アクセスしたユーザーが現在ログインしているユーザーか
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user,only:[:show, :edit, :update]
-  before_action :logged_in_user,only: [:index, :show, :edit, :update]
+  before_action :set_user,only:[:show, :edit, :update, :edit_basic_info, :update_basic_info]
+  before_action :logged_in_user,only: [:index, :show, :edit, :update, :edit_basic_info, :update_basic_info]
   before_action :correct_user,only:[:edit, :update]
   
   def index
@@ -13,13 +13,11 @@ class UsersController < ApplicationController
   
   def show
     # @userにUserテーブルのidが受け取った値である(params[:id])のレコードを見つけて代入
-    @user = User.find(params[:id])
     @first_day = Date.current.beginning_of_month
     @last_day = @first_day.end_of_month
   end
   
   def create
-    @user = User.new(user_params)
     if @user.save
       log_in @user # 保存成功後、ログインします。
       flash[:success] = '新規作成に成功しました。'
@@ -31,26 +29,24 @@ class UsersController < ApplicationController
   
   
   def edit
-   @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
-     if @user.update_attributes(user_params)
-       flash[:success] = "ユーザー情報を更新しました"
-       redirect_to @user
-     else
-      render :edit      
-     end
+   
+       
   end 
   
-def edit_basic_info
-  @user = User.find(params[:id])
-end 
+  def edit_basic_info
+   
 
-def update_basic_info
-  @user = User.find(params[:id])
-end 
+    def update_basic_info
+        if @user.update_attributes(basic_info_params)
+        else
+          flash[:danger] = "更新は失敗しました。" + @user.errors.full_messages.join("<br>")
+        end
+      render edit_basic_info
+    end
+  end  
 
 
    
@@ -63,23 +59,4 @@ end
   def basic_info_params
       params.require(:user).permit(:basic_time, :work_time)
   end
-  
-  def set_user
-    @user = User.find(params[:id])
-  end  
-  
-  # ログインしているか？
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "ログインしてください。"
-      redirect_to login_url
-    end
-  end
-  
-  def correct_user
-    # アクセスしたユーザーが現在ログインしているユーザーか
-    redirect_to(root_url) unless current_user?(@user)
-  end
-end  
-
+end
