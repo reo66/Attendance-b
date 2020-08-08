8daf85f10ce2d14e11592b1b736b328e3dce6ce5
EC2 Default User <ec2-user@ip-172-31-39-96.ap-northeast-1.compute.internal>
2020-08-08 00:21:08 +0000

    set_one_month ok

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -4,4 +4,12 @@ class Attendance < ApplicationRecord
   
   validates :worked_on, presence: true
   validates :note, length: { maximum: 50 }
+  
+  # 出勤時間が存在しない場合、退勤時間は無効
+  validate :finished_at_is_invalid_without_a_started_at
+  
+  def finished_at_is_invalid_without_a_started_at
+    # 出勤時間がからで退勤時間が存在する場合にエラーを出す
+    errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
+  end
 end
