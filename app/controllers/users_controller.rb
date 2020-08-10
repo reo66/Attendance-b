e13908e6cec7c5cc8f46fe4b8e3a95bb4146329d
EC2 Default User <ec2-user@ip-172-31-39-96.ap-northeast-1.compute.internal>
2020-08-10 06:28:31 +0000

    destroy ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
-  before_action :set_user,only:[:show, :edit,:update,:edit_basic_info,:update_basic_info]
-  before_action :logged_in_user,only: [:index,:edit,:update,:edit_basic_info, :update_basic_info]
+  before_action :set_user,only:[:show, :edit,:update,:destroy,:edit_basic_info,:update_basic_info]
+  before_action :logged_in_user,only: [:index,:edit,:update,:destroy,:edit_basic_info, :update_basic_info]
   before_action :correct_user,only:[:edit, :update]
-  before_action :admin_user, only: [:edit_basic_info, :update_basic_info]
+  before_action :admin_user, only: [:destroy,:edit_basic_info, :update_basic_info]
   before_action :set_one_month,only: :show
   
   def index
@@ -41,6 +41,12 @@ class UsersController < ApplicationController
       render :edit      
     end
   end 
+  
+  def destroy
+    @user.destroy
+    flash[:success] = "#{@user.name}のデータを削除しました。"
+    redirect_to users_url
+  end  
   
   def edit_basic_info
   end  
