a406d27656a85fcf7c8159e87eeaf92d1b3f4c0e
EC2 Default User <ec2-user@ip-172-31-39-96.ap-northeast-1.compute.internal>
2020-08-11 12:18:25 +0000

    追加機能8まで

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -7,8 +7,10 @@ class Attendance < ApplicationRecord
   
   # 出勤時間が存在しない場合、退勤時間は無効
   validate :finished_at_is_invalid_without_a_started_at
+  # 退社時間が出社時間より早い時間の場合、無効
   validate :started_at_than_finished_at_fast_if_invalid
   validate :only_started_at
+  
 
  
   
